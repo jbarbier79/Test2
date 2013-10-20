e81de245c8a917be77c534b5589027aee1426717
jbarbier79 <jbarbier79@gmail.com>
2013-10-20 20:53:16 +0200

    second commit

---
+++ Test2Java/src/jb/test2pack/Test2HelloW.java
@@ -0,0 +1,12 @@
+package jb.test2pack;
+
+public class Test2HelloW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("abc");
+		
+		
+	}
+
+}
